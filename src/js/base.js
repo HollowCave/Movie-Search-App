103089a29b7bef0631b85687012129b7972c7fbf
Alex Hval <alexhval@juno.com>
2019-06-25 20:06:44 -0400

    Delete base.js

--- src/js/base.js
+++
@@ -1,6 +0,0 @@
-export const elements ={
-  movieResultList: document.querySelector('.results-list'),
-  topResultList: document.querySelector('.top-results-list'),
-  movie: document.querySelector('.hidden-content'),
-  topMovie: document.querySelector('.hidden-content2')
-}
